8f9e9c14eb9be43e5ced4052f010d62554183ec0
Vedernikov <vedernikovnv@gmail.com>
2018-10-11 19:31:07 +0300

    HW5

---
+++ java/reverse/ReverseFastTest.java
@@ -0,0 +1,10 @@
+package reverse;
+
+/**
+ * @author Georgiy Korneev (kgeorgiy@kgeorgiy.info)
+ */
+public class ReverseFastTest {
+    public static void main(String... args) {
+        new ReverseTest(1000_000).run();
+    }
+}
